37fc97b7fda219d78a97d47771925f70a847b063
Andrew <svt.andrey1631@gmail.com>
2023-01-07 17:31:44 +0200

    lab 4 done

--- lab4/token_auth/index.js
+++ lab4/token_auth/index.js
@@ -1,137 +1,95 @@
-const uuid = require('uuid');
-const express = require('express');
-const onFinished = require('on-finished');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const axios = require("axios");
 const port = 3000;
-const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const SESSION_KEY = 'Authorization';
+app.get("/", async (req, res) => {
+  if (req.headers.authorization) {
+    const token = req.headers.authorization;
+    const userId = getUserId(token);
 
-class Session {
-    #sessions = {}
+    var options = {
+      method: 'GET',
+      url: `https://kpi.eu.auth0.com/api/v2/users/${userId}`,
+      headers: {ContentType: 'application/json', authorization: token}
+    };
+    
+    axios.request(options).then(function (response) {
+      const user = response.data;
+      
+      return res.json({
+        username: user.name,
+        logout: "http://localhost:3000/logout",
+      });
+    }).catch(function (error) {
+      console.error(error);
+    });
+    return;
+  }
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
-    constructor() {
-        try {
-            this.#sessions = fs.readFileSync('./sessions.json', 'utf8');
-            this.#sessions = JSON.parse(this.#sessions.trim());
+app.post("/api/login", async (req, res) => {
+  const { login, password } = req.body;
 
-            console.log(this.#sessions);
-        } catch(e) {
-            this.#sessions = {};
-        }
+  const options = makeAuthOptions(login, password);
+  
+  axios.request(options).then(function (response) {
+    if (response.status === 200) {
+      const { access_token, refresh_token, expires_in } = response.data;
+      res.json({ success: true, access_token });
+      return;
     }
+    res.status(400).json(response.data);
 
-    #storeSessions() {
-        fs.writeFileSync('./sessions.json', JSON.stringify(this.#sessions), 'utf-8');
-    }
+  }).catch(function (error) {
+    if(error.response.statusText === 'Too Many Requests' || error.response.statusText === 'Forbidden') 
+      res.status(error.response.status).json();
+    else
+      console.error(error);
+  });
+});
 
-    set(key, value) {
-        if (!value) {
-            value = {};
-        }
-        this.#sessions[key] = value;
-        this.#storeSessions();
-    }
 
-    get(key) {
-        return this.#sessions[key];
-    }
+function makeAuthOptions(login, password) {
+  var options = {
+    method: 'POST',
+    url: 'https://kpi.eu.auth0.com/oauth/token',
+    headers: {'content-type': 'application/x-www-form-urlencoded'},
+    data: new URLSearchParams({
+      grant_type: 'http://auth0.com/oauth/grant-type/password-realm',
+      audience: 'https://kpi.eu.auth0.com/api/v2/',
+      scope: 'offline_access',
+      client_id: 'JIvCO5c2IBHlAe2patn6l6q5H35qxti0',
+      client_secret: 'ZRF8Op0tWM36p1_hxXTU-B0K_Gq_-eAVtlrQpY24CasYiDmcXBhNS6IJMNcz1EgB',
+      realm: 'Username-Password-Authentication'
+    })
+  };
 
-    init(res) {
-        const sessionId = uuid.v4();
-        this.set(sessionId);
+  options.data.append('username', login);
+  options.data.append('password', password);
 
-        return sessionId;
-    }
-
-    destroy(req, res) {
-        const sessionId = req.sessionId;
-        delete this.#sessions[sessionId];
-        this.#storeSessions();
-    }
+  return options;
 }
 
-const sessions = new Session();
+function getUserId(token) {
+  return parseJwt(token).sub;
+}
 
-app.use((req, res, next) => {
-    let currentSession = {};
-    let sessionId = req.get(SESSION_KEY);
-
-    if (sessionId) {
-        currentSession = sessions.get(sessionId);
-        if (!currentSession) {
-            currentSession = {};
-            sessionId = sessions.init(res);
-        }
-    } else {
-        sessionId = sessions.init(res);
-    }
-
-    req.session = currentSession;
-    req.sessionId = sessionId;
-
-    onFinished(req, () => {
-        const currentSession = req.session;
-        const sessionId = req.sessionId;
-        sessions.set(sessionId, currentSession);
-    });
-
-    next();
-});
-
-app.get('/', (req, res) => {
-    if (req.session.username) {
-        return res.json({
-            username: req.session.username,
-            logout: 'http://localhost:3000/logout'
-        })
-    }
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
-
-app.get('/logout', (req, res) => {
-    sessions.destroy(req, res);
-    res.redirect('/');
-});
-
-const users = [
-    {
-        login: 'Login',
-        password: 'Password',
-        username: 'Username',
-    },
-    {
-        login: 'Login1',
-        password: 'Password1',
-        username: 'Username1',
-    }
-]
-
-app.post('/api/login', (req, res) => {
-    const { login, password } = req.body;
-
-    const user = users.find((user) => {
-        if (user.login == login && user.password == password) {
-            return true;
-        }
-        return false
-    });
-
-    if (user) {
-        req.session.username = user.username;
-        req.session.login = user.login;
-
-        res.json({ token: req.sessionId });
-    }
-
-    res.status(401).send();
-});
+function parseJwt (token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+  return JSON.parse(jsonPayload);
+};
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
